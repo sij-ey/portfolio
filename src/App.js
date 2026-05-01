7d61f60cd4322bf1eb2af0d3846fefd3f8f13633
sij-ey <charlseochieng01@gmail.com>
2026-05-01 14:49:31 +0300

    deploy ready

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Education from "./components/Education";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="dark:bg-gray-900 dark:text-white">
+      <Navbar />
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Skills />
+      <Education />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
